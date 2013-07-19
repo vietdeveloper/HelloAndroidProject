483fec9865186fb0e87018ab2d6a69242a40b82f
Viet La <vietdeveloper@live.com>
2013-07-19 11:59:05 -0700

    Added resource files for Spanish and French

--- HelloAndroid/build/source/r/debug/com/vietdeveloper/helloandroid/R.java
+++ HelloAndroid/build/source/r/debug/com/vietdeveloper/helloandroid/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/dimens.xml 
- From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-sw720dp-land/dimens.xml 
+        /**  From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/dimens.xml 
+ From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-sw720dp-land/dimens.xml 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -30,22 +30,26 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        /**  From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/strings.xml 
+        /**  From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/strings.xml 
          */
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
+        /**  From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-es/strings.xml 
+ From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-fr/strings.xml 
+         */
         public static final int hello_world=0x7f050002;
+        public static final int title=0x7f050003;
     }
     public static final class style {
-        /**  From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/styles.xml 
+        /**  From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values/styles.xml 
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-v11/styles.xml 
+ From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-v11/styles.xml 
  API 11 theme customizations can go here. 
- From: /Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-v14/styles.xml 
+ From: file:/Users/viet/AndroidStudioProjects/HelloAndroidProject/HelloAndroid/src/main/res/values-v14/styles.xml 
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
